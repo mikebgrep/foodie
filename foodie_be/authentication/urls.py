d33d06bc1acbc67732f17f5fe50fd58c1c635327
mikebgrep <mike2016consult@abv.bg>
2024-09-09 10:43:18 +0300

    return signup view in authentication app

--- foodie_be/authentication/urls.py
+++ foodie_be/authentication/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
+from .views import SignUpView
+
 app_name = "authentication"
 
 urlpatterns = [
-
-
+    path("signup", SignUpView.as_view()),
 ]
--- foodie_be/authentication/views.py
+++ foodie_be/authentication/views.py
@@ -1,2 +1,14 @@
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
+
+from .serializers import UserSerializer
+from ..foodie.HeaderAuthentication import HeaderAuthentication
+
+
+class SignUpView(CreateAPIView):
+    authentication_classes = [HeaderAuthentication]
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny, )
+
 
 
