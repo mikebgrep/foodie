5336bd996c5d5924b1dcd197b8bbf4f491bcb8c1
mikebgrep <mike2016consult@abv.bg>
2024-09-09 11:02:31 +0300

    fix Header authentication import

--- foodie_be/authentication/views.py
+++ foodie_be/authentication/views.py
@@ -2,7 +2,7 @@ from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
 
 from .serializers import UserSerializer
-from ..foodie.HeaderAuthentication import HeaderAuthentication
+from foodie.HeaderAuthentication import HeaderAuthentication
 
 
 class SignUpView(CreateAPIView):
