f1c1eaff92ee44bcdeb03770619febb41f4d3b57
mikebgrep <mike2016consult@abv.bg>
2024-09-10 11:34:42 +0300

    fix typo, add favorite action in FavoriteRecpes ednpoint, add route, restring to two methods only GET and PATCH

--- foodie_be/foodie/urls.py
+++ foodie_be/foodie/urls.py
@@ -1,15 +1,19 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import SimpleRouter
 
-from .views import Categories, TrendingRecipies, CategoryRecipes, FavoriteRecipies, Tags, TagsRecipies, SearchRecipies
+from .views import Categories, TrendingRecipies, CategoryRecipes, FavoriteRecipes, Tags, TagsRecipies, SearchRecipies
 
 app_name = "foodie"
+
+route = SimpleRouter()
+route.register(r"favorites", FavoriteRecipes)
 
 urlpatterns = [
     path("category", Categories.as_view()),
     path("trending", TrendingRecipies.as_view()),
     path("category/<int:pk>/recipes", CategoryRecipes.as_view()),
-    path("favorites", FavoriteRecipies.as_view()),
     path("tags", Tags.as_view()),
     path("tag/<int:pk>/recipes", TagsRecipies.as_view()),
-    path("", SearchRecipies.as_view())
+    path("", SearchRecipies.as_view()),
+    path('', include(route.urls)),
 ]
--- foodie_be/foodie/views.py
+++ foodie_be/foodie/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import get_list_or_404
+from django.shortcuts import get_list_or_404, get_object_or_404
 from rest_framework import filters
+from rest_framework.decorators import action
 from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED
+from rest_framework.viewsets import ModelViewSet
 
 from .HeaderAuthentication import HeaderAuthentication
 from .models import Category, Recipe, Tag
@@ -39,10 +43,22 @@ class TrendingRecipies(ListAPIView):
         return Recipe.objects.filter(pk__in=results_pks)
 
 
-class FavoriteRecipies(ListAPIView):
+class FavoriteRecipes(ModelViewSet):
     authentication_classes = [HeaderAuthentication]
+    http_method_names = ['get', 'patch']
     serializer_class = RecipesSerializer
     queryset = Recipe.objects.filter(is_favorite=True)
+
+    @action(detail=True, methods=['patch'])
+    def favorite(self, request, *args, **kwargs):
+        recipe = get_object_or_404(Recipe, pk=kwargs['pk'])
+        if recipe.is_favorite:
+            recipe.is_favorite = False
+        else:
+            recipe.is_favorite = True
+        recipe.save()
+
+        return Response(data="Success", status=HTTP_201_CREATED)
 
 
 class Tags(ListAPIView):
